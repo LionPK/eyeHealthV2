5281a54e67ba33c9389ddfa983f685b31298a24b
LionPK <praneed.kla@northbkk.ac.th>
2018-05-14 16:37:38 +0700

    Initial commit

---
+++ app/src/androidTest/java/a0/eyehealth2/singl/crud/com/eyehealth20/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package a0.eyehealth2.singl.crud.com.eyehealth20
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("a0.eyehealth2.singl.crud.com.eyehealth20", appContext.packageName)
+    }
+}
